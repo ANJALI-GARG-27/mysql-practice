edbb459366dc574bfdf5771a658d6e2a8df329a3
Anjali Garg <anjaligarg8523@gmail.com>
2026-02-23 00:04:21 +0530

    new table added

--- fundamentals.sql
+++ fundamentals.sql
@@ -1,2 +1,9 @@
 create DATABASE student;
 use student;
+CREATE DATABASE dance_club;
+drop DATABASE dance_club;
+use student;
+create table AIDS(
+    Rollno int, Name VARCHAR(30), Age INT
+);
+INSERT into aids VALUES(101,"Naira",20),(102,"Rohan",22),(104,"Anjali",21);
