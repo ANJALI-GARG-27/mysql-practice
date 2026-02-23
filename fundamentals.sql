98319e6e23fd08ce6ec972e84555a1301cf6daa6
Anjali Garg <anjaligarg8523@gmail.com>
2026-02-23 23:02:30 +0530

    where and order by

--- fundamentals.sql
+++ fundamentals.sql
@@ -1,9 +1,43 @@
-create DATABASE student;
-use student;
 CREATE DATABASE dance_club;
 drop DATABASE dance_club;
-use student;
-create table AIDS(
-    Rollno int, Name VARCHAR(30), Age INT
-);
-INSERT into aids VALUES(101,"Naira",20),(102,"Rohan",22),(104,"Anjali",21);
+CREATE DATABASE Instagram;
+use Instagram;
+show databases;
+CREATE Table User(
+    id int PRIMARY KEY,
+    age int ,
+    name VARCHAR(30) NOT NULL,
+    email VARCHAR(50) UNIQUE,
+    followers int default 0,
+    following int,
+    constraint age_check check (age>=13)
+)
+
+insert into user (id, age, name, email, followers, following) 
+VALUES (1,15,"Bob","bob123@gmail.com",200,250),
+(2,14,"Aliza","aliza@yahoo.in",500,360),
+(3,16,"Casey","casey2@gmail.com",200,300),
+(4,14,"Daniel","dan45@gmail.com",140,178),
+(5,17,"Frank","franky@yahoo.in",220,490)
+
+--Using select, where clause and various operators in it 
+select * from user;
+select id ,age,name from user where age>15;
+
+select id , name, followers from user 
+where followers between 300 and 600;
+
+select id,name from user WHERE name like "%a";
+
+select name , age from user where age in (14,16);
+
+select * from user where (following>200 AND followers>200);
+
+select DISTINCT age from user ;
+
+--order by clause
+select * from user order by followers ASC;
+
+select id,name,following from user
+where age>14
+order by following desc;
